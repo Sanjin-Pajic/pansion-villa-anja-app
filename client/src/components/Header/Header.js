95dcd93e0f1835ffe346650d196fe32a71806351
Sanjin-Pajic <pajicsanjo@gmail.com>
2022-09-22 16:57:26 +0200

    header work

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import "./Header.css";
+
+export default function Header() {
+    return (
+        <>
+        <div className="header_wrapper">
+        <header className="main_header_area">
+            <div className="header_top">
+                <a className="phoneNumber">+ (387) 62 339 318</a>
+            </div>
+            <div className="header_bottom_menu"></div>
+        </header>
+        </div>
+        </>
+    )
+}
