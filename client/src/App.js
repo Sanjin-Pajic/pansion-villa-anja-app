c3b11b2238c19e0cdd2e7a0a8837217e3f76d5e9
Sanjin-Pajic <pajicsanjo@gmail.com>
2022-09-24 03:49:21 +0200

    added introduction

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Home from "./components/Home/Home";
 import BookTable from "./components/BookTable/BookTable";
+import Introduction from "./components/Introduction/Introduction"
 
 function App() {
   return (
   <div className="App">
     <Home/>
     <BookTable/>
+    <Introduction/>
   </div>
   )
 }
--- client/src/components/Introduction/Introduction.js
+++ client/src/components/Introduction/Introduction.js
@@ -3,6 +3,7 @@ import "./Introduction.css";
 export default function Introduction() {
     return (
     <div className="introduction_wrapper">
+        <h1 className="introduction_title"><span className="introduction_title_highlighted">Introduction </span>of Villa Anja </h1>
     </div>
     )
 }
