3d0bfc264f26c0d86e53c94e68fe2648a83808fa
Sanjin-Pajic <pajicsanjo@gmail.com>
2022-09-28 01:57:02 +0200

    before changing fonts

--- client/src/components/Introduction/Introduction.js
+++ client/src/components/Introduction/Introduction.js
@@ -3,7 +3,9 @@ import "./Introduction.css";
 export default function Introduction() {
     return (
     <div className="introduction_wrapper">
-        <h1 className="introduction_title"><span className="introduction_title_highlighted">Introduction </span>of Villa Anja </h1><br></br>
+        <h1 className="introduction_title"><span className="introduction_title_highlighted">Introduction </span>of Villa Anja <br></br><h1 className="introduction_title_borderbottom">_______</h1></h1>
+        <p className="introduction_italic_text">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum</p>
+        <p className="introduction_highlighted_text">WE ARE AVAILABLE FOR BUSINESS</p>
     </div>
     )
 }
