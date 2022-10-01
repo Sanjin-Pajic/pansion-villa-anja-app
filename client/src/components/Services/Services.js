08a6a198227454606e7586860081ab0a4e820226
Sanjin-Pajic <pajicsanjo@gmail.com>
2022-10-01 23:36:18 +0200

    services.js complete

--- client/src/components/Services/Services.js
+++ client/src/components/Services/Services.js
@@ -2,6 +2,7 @@ import { requirePropFactory } from "@mui/material";
 import "./Services.css";
 import BakeryDiningIcon from '@mui/icons-material/BakeryDining';
 import LocalParkingIcon from '@mui/icons-material/LocalParking';
+import WifiIcon from '@mui/icons-material/Wifi';
 
 
 export default function Services() {
@@ -13,16 +14,22 @@ export default function Services() {
                 <h1 className="services_subtext">quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam.</h1>
                 <span className="link_view_all_services">View all services</span>
                 </div>
-                <div className="services_bullet1">
-                    <BakeryDiningIcon style={{color: "rgb(204, 130, 0)"}} transform="scale(4)"/>
-                    <p className="bullet1_header_text">Breakfast</p>
-                    <p className="bullet1_subtext">Incidunt ut labore et dolore magnam aliquam quaerat volup tatem. Utad minima.</p>
+                <div className="services_bullet">
+                    <LocalParkingIcon style={{color: "rgb(204, 130, 0)"}} transform="scale(4)"/>
+                    <p className="bullet_header_text">Free Parking</p>
+                    <p className="bullet_subtext">Incidunt ut labore et dolore magnam aliquam quaerat volup tatem. Utad minima.</p>
 
                 </div>
-                <div className="services_bullet2">
-                    <LocalParkingIcon style={{color: "rgb(204, 130, 0)"}} transform="scale(4)"/>
-                    <p className="bullet1_header_text">Parking</p>
-                    <p className="bullet1_subtext">Incidunt ut labore et dolore magnam aliquam quaerat volup tatem. Utad minima.</p>
+                <div className="services_bullet">
+                    <WifiIcon style={{color: "rgb(204, 130, 0)"}} transform="scale(4)"/>
+                    <p className="bullet_header_text">Free Wi-fi</p>
+                    <p className="bullet_subtext">Incidunt ut labore et dolore magnam aliquam quaerat volup tatem. Utad minima.</p>
+
+                </div>
+                <div className="services_bullet">
+                    <BakeryDiningIcon style={{color: "rgb(204, 130, 0)"}} transform="scale(4)"/>
+                    <p className="bullet_header_text">Breakfast</p>
+                    <p className="bullet_subtext">Incidunt ut labore et dolore magnam aliquam quaerat volup tatem. Utad minima.</p>
 
                 </div>
             </div>
